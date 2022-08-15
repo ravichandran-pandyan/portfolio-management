0c48f985490bba322b9f8d10d0a9ccbdd260ce7d
ravichandran-pandyan <ravichandran.pandyan@gmail.com>
2022-08-15 22:46:11 +0530

    junit testing execution handled from command line

--- com/mymoney/PortfolioManagement.java
+++ com/mymoney/PortfolioManagement.java
@@ -16,22 +16,30 @@ Portfolio portfolio;
 public static void main(String[] args){
   PortfolioManagement portfolioManagement = new PortfolioManagement();
   try {
+          if(args.length == 0){
+            System.out.println("File URL is not found");
+            return;
+          }
+
+          String inputStr = args[0];
+
+          if(inputStr.equals("test")){
+              Result result = JUnitCore.runClasses(PortfolioManagementTest.class);
+
+              for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+              }
+
+              System.out.println(result.wasSuccessful());
+              return;
+          }
+
           Scanner scanner = new Scanner(new File(args[0]));
           while (scanner.hasNextLine()) {
-              String inputStr = scanner.nextLine();
+              inputStr = scanner.nextLine();
               portfolioManagement.portfolio = CommandHandler.executeCommand(portfolioManagement.portfolio, inputStr);
           }
 
-          String inputStr = (args.length > 1) ? args[1] : null;
-          if(inputStr != null && inputStr.equals("test")){
-            Result result = JUnitCore.runClasses(PortfolioManagementTest.class);
-
-            for (Failure failure : result.getFailures()) {
-              System.out.println(failure.toString());
-            }
-
-            System.out.println(result.wasSuccessful());
-          }
 
       } catch (IOException e) {
           System.out.println(e.getMessage());
